26f5216ab62a0779391d4e639c3ad29a6e9c4e39
Stefan Lissia <stefan.emailacct@gmail.com>
2015-05-04 00:16:48 +0100

    completed stage 4

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,10 +3,18 @@ require 'docking_station'
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
+  it { is_expected.to respond_to(:dock).with(1).argument }
+
   it 'releases working bikes' do
+    subject.dock Bike.new
     bike = subject.release_bike
     expect(bike).to be_working
   end
-  
+
+  describe 'release_bike' do
+    it 'raises an error when there are no bikes available' do
+      expect { subject.release_bike}.to raise_error 'No bikes available'
+    end
+  end
 
 end
--- spec/features/public_accesses_bike_spec.rb
+++ spec/features/public_accesses_bike_spec.rb
@@ -1,7 +1,15 @@
 feature 'member of public accesses bike' do
   scenario 'docking station releases a working bike' do
     docking_station = DockingStation.new
+    docking_station.dock Bike.new
     bike = docking_station.release_bike
     expect(bike).to be_working
   end
+
+  scenario 'docking station does not release a bike when there are none available' do
+    docking_station = DockingStation.new
+    expect { docking_station.release_bike }.to raise_error 'No bikes available'
+  end
+
+
 end
